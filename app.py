4b5dbe4762097b8226f742911aff60a144dc154d
Frank Moreno <Frank.Moreno95@gmail.com>
2019-01-23 19:02:59 +0000

    Define login/movies route
    Added comments for work that is going to need to be done on each route

--- app.py
+++ app.py
@@ -6,25 +6,18 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def heartBeat():
-    #return getMovies()
     return 'Server is up.'
 
-def getMovies():
-    POST_LOGIN_URL = 'https://sso.noovie.com/login?appKey=fml_web/login-confirm'
-
-    #This URL is the page you actually want to pull down with requests.
-    REQUEST_URL = 'https://fantasymovieleague.com/checkoutmovies'
-
-    payload = {
-        'emailAddress': 'Frank.Moreno95@gmail.com',
-        'password':'
-    }
-
-    with requests.Session() as session:
-        post = session.post(POST_LOGIN_URL, data=payload)
-        r = session.get(REQUEST_URL)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        #return soup.prettify()
-        print(soup.find_all("div", class_="movie-info"))
+@app.route('/login', methods['POST'])
+def login():
+    # PARAMETERS: Username + password
+    # Call login method on fml
+    # RETURN: session/cookies
+    
+@app.route('/movies', methods['GET])
+def returnMovies():
+    # PARAMETERS: Username + password                         
+    # Call get movies with session
+    # RETURN: list of movies in JSON                          
         
 app.run()
