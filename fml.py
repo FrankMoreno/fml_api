b4081fde9d896a48d32cc21543dbd60ce0fbac0f
Frank Moreno <Frank.Moreno95@gmail.com>
2019-01-23 18:43:57 +0000

    Breaking fml file into methods
    Lets me easily import into flask server to get the info

--- fml.py
+++ fml.py
@@ -19,3 +19,33 @@ with requests.Session() as session:
         Movies[name] = bux
 
 print(Movies)
+
+# After this line is work in progress
+def login(email, password):
+    LOGIN_URL = 'https://sso.noovie.com/login?appKey=fml_web/login-confirm'
+    payload = {
+        'emailAddress': email,
+        'password': password
+    }
+    
+    session = requests.Session()
+    session.post(LOGIN_URL, data=payload)
+    
+    return session
+
+def getMovies(session):
+    MOVIES_URL = 'https://fantasymovieleague.com/checkoutmovies'
+    Movies = {}
+    
+    response = session.get(MOVIES_URL)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for movieRow in soup.find("tbody").contents:
+        bux = movieRow.find("td",class_='movie-price numeric stat sorted first').get_text()
+        name = movieRow.find("td",class_='movie-info').div.div.h3.get_text()
+        Movies[name] = bux
+        
+    return Movies
+
+def getLeagues(session):
+    LEAGUES_URL = 'https://fantasymovieleague.com/league/directory'
+    Leagues = {}
