e56d560140f108fbd627b26c4295b5d3e56bf9b5
Frank Moreno <Frank.Moreno95@gmail.com>
2019-01-23 19:39:31 -0600

    Full seperated into methods and pulled personal league info

--- fml.py
+++ fml.py
@@ -1,36 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-LOGIN_URL = 'https://sso.noovie.com/login?appKey=fml_web/login-confirm'
-MOVIES_URL = 'https://fantasymovieleague.com/checkoutmovies'
-payload = {
-    'emailAddress': '',
-    'password': ''
-}
-Movies = {}
-
-with requests.Session() as session:
-    session.post(LOGIN_URL, data=payload)
-    r = session.get(MOVIES_URL)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    for movieRow in soup.find("tbody").contents:
-        bux = movieRow.find("td",class_='movie-price numeric stat sorted first').get_text()
-        name = movieRow.find("td",class_='movie-info').div.div.h3.get_text()
-        Movies[name] = bux
-
-print(Movies)
-
-# After this line is work in progress
 def login(email, password):
-    LOGIN_URL = 'https://sso.noovie.com/login?appKey=fml_web/login-confirm'
+    LOGIN_URL = 'https://fantasymovieleague.com/auth/loginconfirm'
     payload = {
         'emailAddress': email,
         'password': password
     }
     
     session = requests.Session()
-    session.post(LOGIN_URL, data=payload)
-    
+    response = session.post(LOGIN_URL, data=payload)
     return session
 
 def getMovies(session):
@@ -38,7 +17,7 @@ def getMovies(session):
     Movies = {}
     
     response = session.get(MOVIES_URL)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
     for movieRow in soup.find("tbody").contents:
         bux = movieRow.find("td",class_='movie-price numeric stat sorted first').get_text()
         name = movieRow.find("td",class_='movie-info').div.div.h3.get_text()
@@ -50,8 +29,26 @@ def getLeagues(session):
     LEAGUES_URL = 'https://fantasymovieleague.com/league/directory'
     Leagues = {}
 
+    response = session.get(LEAGUES_URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    leagueRows = soup.find("table",class_='tableType-league noLeagues').tbody
+    for leagueRow in leagueRows.find_all("tr"):
+        print(leagueRow.find("td",class_='league-name first').a.get_text())
+        # TO-DO get href
+        # TO-DO get rankings
+    
+    return Leagues
+
 def submitPicks():
+    Picks = {}
     
 def getPicks():
-    
+    Picks = {}
 
+def main():
+    session = login("Frank.Moreno95@gmail.com", "Tacos123")
+    getLeagues(session)
+    getMovies(session)
+
+if __name__ == '__main__':
+    main()
