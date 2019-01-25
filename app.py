7abc1845dc9d86f57ffc2ca81322260e7ced80e1
Frank Moreno <Frank.Moreno95@gmail.com>
2019-01-24 18:19:41 -0600

    Add some TODOs and remove unnecessary comments

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def heartBeat():
 
 @app.route('/login', methods=['GET'])
 def login():
+    # TODO Add check if user is already logged in
     newCookies = fml.login('Frank.Moreno95@gmail.com','*******')
     session['cookies'] = json.dumps(newCookies)
     return 'You logged in'
@@ -20,20 +21,14 @@ def login():
     
 @app.route('/movies', methods=['GET'])
 def returnMovies():
-    # TO-DO add check for cookies
+    # TODO add check for cookies
     movies = fml.getMovies(session['cookies'])
     return jsonify(movies)
-    # PARAMETERS: Username + password                         
-    # Call get movies with session
-    # RETURN: list of movies in JSON
 
 @app.route('/leagues', methods=['GET'])
 def returnLeagues():
-    # TO-DO add check for cookies
+    # TODO add check for cookies
     leagues = fml.getLeagues(session['cookies'])
-    return jsonify(leagues)
-    # PARAMETERS: Username + password                         
-    # Call get movies with session
-    # RETURN: list of movies in JSO                          
+    return jsonify(leagues)                         
         
 app.run(debug=True)
