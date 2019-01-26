a457b2fed5e0ef72ebc9c6bfef75583af2551ca7
Frank Moreno <Frank.Moreno95@gmail.com>
2019-01-26 16:23:42 -0600

    Add CORS to solve fetch issue

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, session, jsonify
-import requests
+from flask_cors import CORS
 import fml
 import json
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/', methods=['GET'])
 def heartBeat():
     return 'Server is up.'
